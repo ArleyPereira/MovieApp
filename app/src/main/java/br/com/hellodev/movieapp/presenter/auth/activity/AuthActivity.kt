b867646ef51d85b03a46f58a9266ba83b19b1b4d
Arley Santana <arley1995@ucl.br>
2023-04-23 14:16:10 -0300

    32 - Configurando StatusBar

--- app/src/main/java/br/com/hellodev/movieapp/presenter/auth/activity/AuthActivity.kt
+++ app/src/main/java/br/com/hellodev/movieapp/presenter/auth/activity/AuthActivity.kt
@@ -1,15 +1,48 @@
 package br.com.hellodev.movieapp.presenter.auth.activity
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import br.com.hellodev.movieapp.R
+import br.com.hellodev.movieapp.databinding.ActivityAuthBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class AuthActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var navHostFragment: NavHostFragment
+
+    private lateinit var binding: ActivityAuthBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setStatusBarTranslucent()
+
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        navController.addOnDestinationChangedListener{ _, destination, _ ->
+            if(destination.id != R.id.onboardingFragment){
+                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            }
+        }
+    }
+
+    private fun setStatusBarTranslucent() {
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
     }
 
 }
