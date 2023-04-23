fe06a5697f9d55112ea57d476ee39ad5ba669011
Arley Santana <arley1995@ucl.br>
2023-04-23 15:34:42 -0300

    34 - Verificando usuário logado

--- app/src/main/java/br/com/hellodev/movieapp/presenter/auth/activity/AuthActivity.kt
+++ app/src/main/java/br/com/hellodev/movieapp/presenter/auth/activity/AuthActivity.kt
@@ -1,5 +1,6 @@
 package br.com.hellodev.movieapp.presenter.auth.activity
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +8,8 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import br.com.hellodev.movieapp.R
 import br.com.hellodev.movieapp.databinding.ActivityAuthBinding
+import br.com.hellodev.movieapp.presenter.main.activity.MainActivity
+import br.com.hellodev.movieapp.util.FirebaseHelper
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -25,14 +28,17 @@ class AuthActivity : AppCompatActivity() {
         setStatusBarTranslucent()
 
         initNavigation()
+
+        isAuthenticated()
     }
 
     private fun initNavigation() {
-        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
-        navController.addOnDestinationChangedListener{ _, destination, _ ->
-            if(destination.id != R.id.onboardingFragment){
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id != R.id.onboardingFragment) {
                 window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
             }
         }
@@ -43,6 +49,13 @@ class AuthActivity : AppCompatActivity() {
             View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
                     View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
                     View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+    }
+
+    private fun isAuthenticated() {
+        if (FirebaseHelper.isAuthenticated()) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
     }
 
 }
--- app/src/main/java/br/com/hellodev/movieapp/presenter/main/activity/MainActivity.kt
+++ app/src/main/java/br/com/hellodev/movieapp/presenter/main/activity/MainActivity.kt
@@ -1,19 +1,13 @@
 package br.com.hellodev.movieapp.presenter.main.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
-import br.com.hellodev.movieapp.BuildConfig
 import br.com.hellodev.movieapp.R
 import br.com.hellodev.movieapp.databinding.ActivityMainBinding
-import br.com.hellodev.movieapp.presenter.auth.forgot.ForgotFragment
-import br.com.hellodev.movieapp.presenter.auth.login.LoginFragment
-import br.com.hellodev.movieapp.presenter.auth.register.RegisterFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -30,8 +24,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         initNavigation()
-
-        BuildConfig.API_KEY
     }
 
     private fun initNavigation() {
